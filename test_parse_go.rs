72463da77a947522c996227d86d437436fa1abf9
Ichsan Rahardianto <ichsan.rahardianto@gmail.com>
2026-03-31 18:33:36 +0700

    Stop tracking .agents

--- test_parse_go.rs
+++
@@ -1,8 +0,0 @@
-use tree_sitter::Parser;
-fn main() {
-    let mut parser = Parser::new();
-    parser.set_language(&tree_sitter_go::LANGUAGE.into()).unwrap();
-    let src = "type Storage interface { Create() error }";
-    let tree = parser.parse(src, None).unwrap();
-    println!("{}", tree.root_node().to_sexp());
-}
